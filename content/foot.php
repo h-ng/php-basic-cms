0ec873273b8813ee7496224ba127e49865054184
Hayden Nicholson Goult <54115165+h-ng@users.noreply.github.com>
2020-08-13 17:55:06 +0100

    Update foot.php

--- content/foot.php
+++ content/foot.php
@@ -15,4 +15,11 @@
 <font size="-2">Made by <?php echo $_HC['devper'];?> on behalf of <?php echo $_HC['devco'];?> (Version <?php echo $_HC['version'];?>)
 </font>
 <?php } else { echo ""; }?>
+<?php if($_HC['showdebug'] == "yes") {?>
+<?php $url1 = parse_url($_SERVER['REQUEST_URI']);?>
+<hr>
+</center>
+<pre>Recieved debug information:</pre>
+<?php echo '<pre>'; print_r($url1); echo '</pre>'; ?>
+<?php } else {}?>
 </center>
