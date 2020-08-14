0ddbf5c94b57df287a0bb7827bd4e3024dbaa233
Hayden Nicholson Goult <54115165+h-ng@users.noreply.github.com>
2020-08-14 12:41:05 +0100

    Removed [execss] <?php and ?>

--- index.php
+++ index.php
@@ -4,12 +4,8 @@
   include "content/head.php";
   include "content/meta.php";
   include "content/head1.php";
-?>
-<?php
   include "content/body.php";
   include "content/body1.php";
-?>
-<?php
   include "content/foot.php";
   include "content/foot1.php";
 ?>
