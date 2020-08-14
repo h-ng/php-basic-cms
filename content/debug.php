7b90d02923b5d7e93965f6e1838e1411db00e46e
Hayden Nicholson Goult <54115165+h-ng@users.noreply.github.com>
2020-08-14 12:28:23 +0100

    Update debug.php

--- content/debug.php
+++ content/debug.php
@@ -1,5 +1,11 @@
 <?php
-$visitip = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$split = explode(".", $ip);
+$ip1 = $split[0];
+$ip2 = $split[2];
+$ip3 = $split[3];
+$ip4 = "$ip1.xxx.$ip2.$ip3";
+
 $srvip = $_SERVER['SERVER_ADDR'];
 $httpver = $_SERVER['SERVER_PROTOCOL'];
 $method = $_SERVER['REQUEST_METHOD'];
@@ -7,7 +13,7 @@ $access = $_SERVER['SCRIPT_URI'];
 
 $_HC['debug'] = array (
       "visitip" => "$visitip",
-      "srvip" => "$srvip",
+      "srvip" => "$ip4",
       "visit" => "$httpver $method",
       "page" => "$access"
       );
